35791d146b95dfab47eac208164b2a1fe7038806
Logeshwaran <78139943+logeshwaran10@users.noreply.github.com>
2024-09-05 10:17:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 //Dependencies
-import React, { useState } from 'react';
-  
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom"; // Removed `Switch`
+
 //CSS
-import './App.css';
-import './style/style.scss';
+import "./App.css";
+import "./style/style.scss";
 
 //Components
-import Header from './container/Header';
-import Footer from './container/Footer';
-
+import Header from "./container/Header";
+import Footer from "./container/Footer";
 import AppRouter from "./route/appRouter";
+
 const App = () => {
-  const [selectedContent, setSelectedContent] = useState('pageOne');
+  const [active, setActive] = useState("pageOne");
+  const location = useLocation();
+
+  useEffect(() => {
+    setActive(location.pathname);
+  }, []);
 
   const handleChange = (value) => {
-    setSelectedContent(value)
-  }
+    setActive(value);
+  };
 
   return (
     <div className="app">
-      <Header onSelect={handleChange} />
+      <Header onSelect={handleChange} active={active} />
       <main className="main-content">
         <AppRouter />
       </main>
       <Footer />
     </div>
   );
+
 };
 
 export default App;
