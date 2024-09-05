92ef326c570190f23f2c3c4276b0bae85d88c53e
Logeshwaran <78139943+logeshwaran10@users.noreply.github.com>
2024-09-05 10:17:23 +0530

    Update appRouter.jsx

--- src/route/appRouter.jsx
+++ src/route/appRouter.jsx
@@ -1,23 +1,20 @@
 //Dependencies
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 //Helper
-import routes from './routes';
+import routes from "./routes";
 
 //component
 
 function AppRouter() {
-    return (
-
-        <Routes>
-            {routes.map(route => (
-                <Route key={route?.path} path={route.path} element={route.component} />
-            ))}
-        </Routes>
-    );
+  return (
+    <Routes>
+      {routes.map((route) => (
+        <Route key={route?.path} path={route.path} element={route.component} />
+      ))}
+    </Routes>
+  );
 }
 
 export default AppRouter;
